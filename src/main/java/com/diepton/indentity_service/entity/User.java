1c8f93769d886e515431d23318c2bc2178ed0922
dieptonn <diep.bt194505@sis.hust.edu.vn>
2024-09-09 00:14:42 +0700

    first commit

---
+++ src/main/java/com/diepton/indentity_service/entity/User.java
@@ -0,0 +1,70 @@
+package com.diepton.indentity_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private long id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private LocalDate dayOfBirth;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDayOfBirth() {
+        return dayOfBirth;
+    }
+
+    public void setDayOfBirth(LocalDate dayOfBirth) {
+        this.dayOfBirth = dayOfBirth;
+    }
+}
---
+++ src/main/java/com/diepton/indentity_service/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.diepton.indentity_service.repository;
+
+import com.diepton.indentity_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+}
